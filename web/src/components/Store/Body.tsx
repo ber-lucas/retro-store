13d32ee74c80d7316e7dfab7819d0eb3d7d1d043
Vinícius <viniborg25@gmail.com>
2022-10-12 17:05:23 -0300

    Substituindo o componente Game pelo GameBanner na Store

--- web/src/components/Store/Body.tsx
+++ web/src/components/Store/Body.tsx
@@ -1,29 +1,28 @@
-import React from "react";
-import Game from "./Game";
+import GameBanner from "../GameBanner";
 
 const Body = () => {
   return (
     <div className="bg-zinc-400/20 grid grid-cols-6 gap-6 mt-5 px-[80px] py-8 rounded-lg">
-        <Game imagePath="/game-1.png" name="League of Legends" />
-        <Game imagePath="/game-2.png" name="Dota 2" />
-        <Game imagePath="/game-3.png" name="Counter Strike" />
-        <Game imagePath="/game-4.png" name="Apex Legends" />
-        <Game imagePath="/game-5.png" name="Fortnite" />
-        <Game imagePath="/game-6.png" name="World of Warcraft" />
+        <GameBanner imagePath="/game-1.png" name="League of Legends" />
+        <GameBanner imagePath="/game-2.png" name="Dota 2" />
+        <GameBanner imagePath="/game-3.png" name="Counter Strike" />
+        <GameBanner imagePath="/game-4.png" name="Apex Legends" />
+        <GameBanner imagePath="/game-5.png" name="Fortnite" />
+        <GameBanner imagePath="/game-6.png" name="World of Warcraft" />
 
-        <Game imagePath="/game-1.png" name="League of Legends" />
-        <Game imagePath="/game-2.png" name="Dota 2" />
-        <Game imagePath="/game-3.png" name="Counter Strike" />
-        <Game imagePath="/game-4.png" name="Apex Legends" />
-        <Game imagePath="/game-5.png" name="Fortnite" />
-        <Game imagePath="/game-6.png" name="World of Warcraft" />
+        <GameBanner imagePath="/game-1.png" name="League of Legends" />
+        <GameBanner imagePath="/game-2.png" name="Dota 2" />
+        <GameBanner imagePath="/game-3.png" name="Counter Strike" />
+        <GameBanner imagePath="/game-4.png" name="Apex Legends" />
+        <GameBanner imagePath="/game-5.png" name="Fortnite" />
+        <GameBanner imagePath="/game-6.png" name="World of Warcraft" />
         
-        <Game imagePath="/game-1.png" name="League of Legends" />
-        <Game imagePath="/game-2.png" name="Dota 2" />
-        <Game imagePath="/game-3.png" name="Counter Strike" />
-        <Game imagePath="/game-4.png" name="Apex Legends" />
-        <Game imagePath="/game-5.png" name="Fortnite" />
-        <Game imagePath="/game-6.png" name="World of Warcraft" />
+        <GameBanner imagePath="/game-1.png" name="League of Legends" />
+        <GameBanner imagePath="/game-2.png" name="Dota 2" />
+        <GameBanner imagePath="/game-3.png" name="Counter Strike" />
+        <GameBanner imagePath="/game-4.png" name="Apex Legends" />
+        <GameBanner imagePath="/game-5.png" name="Fortnite" />
+        <GameBanner imagePath="/game-6.png" name="World of Warcraft" />
     </div>
   );
 };
