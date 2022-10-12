baa987ca3ca33590250118f929f8344cdce93a5a
Vinícius <viniborg25@gmail.com>
2022-10-12 17:03:02 -0300

    Adicionando a Store às rotas da aplicação

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route} from 'react-router-dom';
 
 import Landing from './pages/Landing';
+import Store from './pages/Store';
 
 const MainRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Landing />}/>
+            <Route path='/store' element={<Store />}/>
         </Routes>
     )
 }
