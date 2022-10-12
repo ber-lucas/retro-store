ae71f4e3a7b20b916bd9bc858079fe7c64f7faaa
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2022-10-12 17:32:11 -0300

    radix-ui instalado/nav feita ao estilo DropdownMenu/header atualizado

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,13 +1,11 @@
-import { List } from "phosphor-react";
+import CreateDropdownMenu from "./CreateDropdownMenu";
 
 const Header = () => {
     return (
         <header className="w-[72rem] mt-8 flex justify-between items-center">
             <a href=""><img src="/logo-com-degrade.png" alt="logo" /></a>
 
-            <div className="w-16 h-10 rounded bg-red-500 flex justify-center items-center">
-                <List size={32} weight={"bold"} color={'#fff'} />
-            </div>
+            <CreateDropdownMenu/>
         </header>
     )
 }
