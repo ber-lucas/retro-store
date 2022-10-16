f6768fda91b8eae20809a0c48ee990af739b0a79
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2022-10-15 21:57:53 -0300

    pequenas atualizações no espaçamento do header nas páginas

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,7 +2,7 @@ import CreateDropdownMenu from "./CreateDropdownMenu";
 
 const Header = () => {
     return (
-        <header className="w-[72rem] mt-8 flex justify-between items-center">
+        <header className="w-[72rem] flex justify-between items-center">
             <a href=""><img src="/logo-com-degrade.png" alt="logo" /></a>
             <CreateDropdownMenu/>
         </header>
