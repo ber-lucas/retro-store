e374efe57a7d853a0cc02a6a38974ccd3f9647a4
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2022-10-18 16:44:07 -0300

    bug no cart consertado

--- web/src/pages/Cart.tsx
+++ web/src/pages/Cart.tsx
@@ -5,7 +5,7 @@ function Cart() {
     return(
         <div className="max-w-[1360px] mx-auto flex flex-col items-center gap-16 py-8">
             <Header />
-            <CartInfo />
+            <CartInfo title={""} bannerUrl={""} tag={""} price={0} />
         </div>
     )
 }
